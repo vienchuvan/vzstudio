ee1b5b8aa2bdb75941fa052c8e9de062021b765e
VienChuVan <vienchelsea.it@gmail.com>
2025-03-21 01:11:04 +0700

    vienday

--- src/components/Dboard/DboardBaiViet.vue
+++ src/components/Dboard/DboardBaiViet.vue
@@ -6,10 +6,7 @@
           <h3>Chi tiết</h3>
         </div>
 
-        <div
-          class="form-group input-group d-flex"
-          style="flex-direction: column"
-        >
+        <div class="form-group input-group d-flex" style="flex-direction: column">
           <span class="has-float-label">
             <input
               class="form-control"
@@ -17,16 +14,17 @@
               type="text"
               placeholder="Nhập tiêu đề..."
             />
-            <label style="font-size: 16px; font-weight: 600;" >Tiêu đề</label>
+            <label style="font-size: 16px; font-weight: 600">Tiêu đề</label>
           </span>
           <span class="has-float-label mt-4">
             <input
-              class="form-control" style="height: 100px;"
+              class="form-control"
+              style="height: 100px"
               id="last"
               type="text"
               placeholder="Nhập nội dung tóm tắt.."
             />
-            <label style="font-size: 16px; font-weight: 600;" >Nội dung tóm tắt</label>
+            <label style="font-size: 16px; font-weight: 600">Nội dung tóm tắt</label>
           </span>
 
           <span class="has-float-label mt-4">
@@ -36,27 +34,17 @@
               type="text"
               placeholder="Nhập url ảnh..."
             />
-            <label style="font-size: 16px; font-weight: 600;" >url Ảnh hiển thị</label>
+            <label style="font-size: 16px; font-weight: 600">url Ảnh hiển thị</label>
           </span>
 
           <span class="has-float-label mt-4">
-            <input
-              class="form-control"
-              id="last"
-              type="text"
-              placeholder="Surname"
-            />
-            <label style="font-size: 16px; font-weight: 600;" >url</label>
+            <input class="form-control" id="last" type="text" placeholder="Surname" />
+            <label style="font-size: 16px; font-weight: 600">url</label>
           </span>
 
           <span class="has-float-label mt-4">
-            <input
-              class="form-control"
-              id="last"
-              type="text"
-              placeholder="Surname"
-            />
-            <label style="font-size: 16px; font-weight: 600;" >url</label>
+            <label style="font-size: 16px; font-weight: 600">Nội dung bài viết</label>
+            <textarea id="editor" style="height: 500px;"></textarea>
           </span>
         </div>
       </div>
@@ -67,37 +55,13 @@
           <i class="bx bx-plus icon"></i>
           <i class="bx bx-filter"></i>
         </div>
-        <ul
-          class="todo-list"
-          v-for="(itemBaiViet, index) in listBaiViet"
-          :key="index"
-        >
+        <ul class="todo-list" v-for="(itemBaiViet, index) in listBaiViet" :key="index">
           <li class="completed">
             <strong>{{ decodeBase64(itemBaiViet.title) }}</strong>
             <span class="ml-lg-2 cursor-pointer">
-              <i
-                class="fa fa-times-circle"
-                style="color: red"
-                aria-hidden="true"
-              ></i>
+              <i class="fa fa-times-circle" style="color: red" aria-hidden="true"></i>
             </span>
           </li>
-          <!-- <li class="completed">
-            <p>Manage Delivery Team</p>
-            <i class="bx bx-dots-vertical-rounded"></i>
-          </li>
-          <li class="not-completed">
-            <p>Contact Selma: Confirm Delivery</p>
-            <i class="bx bx-dots-vertical-rounded"></i>
-          </li>
-          <li class="completed">
-            <p>Update Shop Catalogue</p>
-            <i class="bx bx-dots-vertical-rounded"></i>
-          </li>
-          <li class="not-completed">
-            <p>Count Profit Analytics</p>
-            <i class="bx bx-dots-vertical-rounded"></i>
-          </li> -->
         </ul>
       </div>
     </div>
@@ -106,27 +70,47 @@
 
 <script>
 import { getBaiViet } from "@/assets/js/snapService";
+
 export default {
+ 
   data() {
     return {
       listBaiViet: "",
+      editor: '',
+      noiDung: "", // Không dùng ref() trong data()
     };
   },
-  mounted() {
-    this.getListBaiViet();
+  async mounted() {
+    await this.getListBaiViet();
+    setTimeout(() => {
+      if (window.ClassicEditor) {
+        // eslint-disable-next-line no-undef
+        ClassicEditor.create(document.querySelector("#editor"), {
+          height: 500, // Điều chỉnh chiều cao tại đây (đơn vị là pixel)
+        })
+          .then(editor => {
+            console.log("CKEditor đã khởi tạo!", editor);
+          })
+          .catch(error => {
+            console.error("Lỗi CKEditor:", error);
+          });
+      } else {
+        console.error("CKEditor chưa được tải!");
+      }
+    }, 500);
   },
   methods: {
     async getListBaiViet() {
       try {
         const resBaiViet = await getBaiViet();
         this.listBaiViet = resBaiViet.response;
-        console.log(" listBaiViet " + this.listBaiViet);
+        console.log("listBaiViet:", this.listBaiViet);
       } catch (err) {
         console.log(err);
       }
     },
     decodeBase64(encodedString) {
-      var decode = atob(encodedString);
+      let decode = atob(encodedString);
       decode = decodeURIComponent(escape(decode));
       return decode;
     },
@@ -135,7 +119,13 @@ export default {
 </script>
 
 <style scoped>
-.has-float-label .form-control:placeholder-shown:not(:focus)+*{
-    top: 13px;
+.has-float-label .form-control:placeholder-shown:not(:focus) + * {
+  top: 13px;
+}
+
+.ck-editor__editable {
+  min-height: 300px !important; /* Điều chỉnh chiều cao */
+  width: 100% !important; /* Đảm bảo chiếm toàn bộ chiều rộng */
+  max-width: 100% !important; /* Giới hạn chiều rộng không vượt quá container */
 }
 </style>
