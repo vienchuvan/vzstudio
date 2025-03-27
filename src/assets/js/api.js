c0ef1ebf47528e12ab83b438e7b2872751712bcb
VienChuVan <vienchelsea.it@gmail.com>
2025-03-27 22:04:01 +0700

    vienday

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,3 +1,6 @@
 var ip = "http://103.200.23.120:3000/";
+var localhost = "http://localhost:3000/";
 export const apiLogin = ip + "api/auth/login";
 export const apiGetLayBaiViet = ip + "api/quantri/services/getBaiViet";
+
+export const apiQuanTriBaiViet = localhost + "api/quantri/services/apiBaiViet";
