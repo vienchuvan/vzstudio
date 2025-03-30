4e056a457db5d80dd21d03816d704ba7319b3185
unknown <vienchu810>
2025-03-30 23:55:34 +0700

    vienday

--- src/components/BlogGioiThieu.vue
+++ src/components/BlogGioiThieu.vue
@@ -1,10 +1,72 @@
 <template>
-   
-    <div class="wrapper row3">
-      <main class="hoc container clear bv">
-        <div style="min-height: 500px" class="padding-top15">
-          <div class="title-bai-viet" style="color: white;" v-html="titleHeThong"></div>
-          <div v-html="contentHeThong" ></div>
+  <header class="header-dv-crop" :class="{ active: isScrolled }" data-header>
+    <div class="container-dv-crop">
+      <div
+        class="overlay"
+        :class="{ active: isNavOpen }"
+        @click="toggleNav"
+        data-overlay
+      ></div>
+
+      <a href="#">
+        <img style="width: 70px; height: 70px" :src="imageUrls.logoCty" />
+      </a>
+
+      <nav class="navbar-dv-crop" :class="{ active: isNavOpen }" data-navbar>
+        <div class="navbar-top">
+          <!-- <a href="#" class="logo">Ted Martins</a> -->
+          <button
+            class="nav-close-btn"
+            aria-label="Close Menu"
+            @click="toggleNav"
+            data-nav-close-btn
+          >
+            <ion-icon name="close-outline"></ion-icon>
+          </button>
+        </div>
+
+        <ul class="navbar-list">
+          <li class="navbar-item">
+            <a href="#gioiThieu" class="navbar-link" data-navbar-link>Giới thiệu</a>
+          </li>
+
+          <li class="navbar-item">
+            <a href="#about" class="navbar-link" data-navbar-link>Danh mục</a>
+          </li>
+
+          <li class="navbar-item">
+            <a href="#services" class="navbar-link" data-navbar-link>Dịch vụ</a>
+          </li>
+
+          <li class="navbar-item">
+            <a href="#features" class="navbar-link" data-navbar-link>Tư Vấn</a>
+          </li>
+
+          <li class="navbar-item">
+            <a href="#blog" class="navbar-link" data-navbar-link>Liên hệ</a>
+          </li>
+
+          <!-- <li class="navbar-item">
+            <a href="#" class="navbar-link" data-navbar-link>Contact Us</a>
+          </li> -->
+        </ul>
+      </nav>
+
+      <button
+        class="nav-open-btn"
+        aria-label="Open Menu"
+        @click="toggleNav"
+        data-nav-open-btn
+      >
+        <ion-icon name="menu-outline"></ion-icon>
+      </button>
+    </div>
+  </header>
+    <div class="wrapper row3 mt-5 " style="padding-top: 15px;">
+      <main class="hoc container clear bv mt-5">
+        <div style="min-height: 500px mt-5" class="padding-top15">
+          <div class="title-bai-viet" style="color: white; font-size: 30px; font-weight: 600;" v-html="titleHeThong"></div>
+          <div v-html="contentHeThong" class="conntent-bai-viet mt-4"></div>
         </div>
       </main>
     </div>
@@ -14,6 +76,7 @@
 import { apiQuanTriBaiViet } from '@/assets/js/api';
 import { decodeBase64 } from '@/assets/js/SnapWeb';
 import axios from 'axios';
+import { imageUrls } from '@/assets/js/imgUrl';
 
  
   export default {
@@ -22,7 +85,7 @@ import axios from 'axios';
     data() {
       return {
         isMenuVisible: false,
-  
+        imageUrls:imageUrls,
         lang: "",
         base64: "",
         base64Title: "",
@@ -75,7 +138,12 @@ import axios from 'axios';
        console.log(" this.base64Title " ,  response.data.result[0].title );
        
        this.titleHeThong = this.decodeBase64(this.base64Title);
-          this.contentHeThong= this.decodeBase64(this.base64);
+       this.$nextTick(() => {
+  this.contentHeThong = `<div class="content-bai-viet">` + 
+    this.decodeBase64(this.base64).replace(/style="[^"]*"/g, "") + 
+    `</div>`;
+});
+
   
 
           console.log("this.titleHeThong ", this.titleHeThong)
@@ -95,5 +163,23 @@ import axios from 'axios';
   .f-s--title {
     font-size: 16.4px;
   }
+  ::v-deep(.content-bai-viet){
+    color: white;
+    text-align: justify  !important;
+  }
+  ::v-deep(.content-bai-viet h3),
+  ::v-deep(.content-bai-viet h2),
+  ::v-deep(.content-bai-viet a),
+  ::v-deep(.content-bai-viet div),
+
+  ::v-deep(.content-bai-viet p) {
+    text-align: justify  !important;
+      color: white !important;
+  }
+  ::v-deep(.content-bai-viet img) {
+    width: 100% !important;
+  }
+  
+
   </style>
   
